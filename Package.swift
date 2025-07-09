59d96a615216c22ec01fa99b6083de43729fb587
new <new@news-Mac-mini.local>
2025-07-09 13:19:36 +0900

    update name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "G1BleManager",
+    name: "G1BLEManager",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "G1BleManager",
-            targets: ["G1BleManager"]),
+            name: "G1BLEManager",
+            targets: ["G1BLEManager"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "G1BleManager"),
+            name: "G1BLEManager"),
 
     ]
 )
